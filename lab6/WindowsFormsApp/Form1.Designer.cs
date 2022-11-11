7939e89e838584cddd37375ffd7ec06d1fc678fa
mirozy13 <korneeva.111@mail.ru>
2022-11-11 19:16:24 +0700

    add new lab6

---
+++ lab6/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleOpenGlControl1 = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // simpleOpenGlControl1
+            // 
+            this.simpleOpenGlControl1.AccumBits = ((byte)(0));
+            this.simpleOpenGlControl1.AutoCheckErrors = false;
+            this.simpleOpenGlControl1.AutoFinish = false;
+            this.simpleOpenGlControl1.AutoMakeCurrent = true;
+            this.simpleOpenGlControl1.AutoSwapBuffers = true;
+            this.simpleOpenGlControl1.BackColor = System.Drawing.Color.Black;
+            this.simpleOpenGlControl1.ColorBits = ((byte)(32));
+            this.simpleOpenGlControl1.DepthBits = ((byte)(16));
+            this.simpleOpenGlControl1.Location = new System.Drawing.Point(-1, 0);
+            this.simpleOpenGlControl1.Name = "simpleOpenGlControl1";
+            this.simpleOpenGlControl1.Size = new System.Drawing.Size(683, 609);
+            this.simpleOpenGlControl1.StencilBits = ((byte)(0));
+            this.simpleOpenGlControl1.TabIndex = 0;
+            this.simpleOpenGlControl1.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            this.simpleOpenGlControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.paint);
+            this.simpleOpenGlControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 607);
+            this.Controls.Add(this.simpleOpenGlControl1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "3D";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl simpleOpenGlControl1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ lab6/WindowsFormsApp/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            simpleOpenGlControl1.InitializeContexts();
+            Gl.glViewport(0, 0, simpleOpenGlControl1.Width, simpleOpenGlControl1.Height);
+            Gl.glClearColor(1, 1, 1, 1);
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);//устанавливаем текущей матрицу проекций
+            Gl.glFrustum(-1, 1, -1, 1, 0, 10);//задаем перспективу
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);//устанавливаем текущей видовую матрицу
+            Gl.glTranslatef(0f, 0f, -3f);//сдвигаем фигуру в центр - после проецирования из
+           
+            Gl.glLoadIdentity();
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glPushMatrix();
+            draw();
+            draw1();
+            simpleOpenGlControl1.Invalidate();
+        }
+
+        void draw1()
+        {
+
+            //пирамида
+            Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_FILL);
+
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            Gl.glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.0f, 0.4f);//вершина
+            Gl.glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(-0.48f, 0.15f, -0.3f);//2
+            Gl.glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0f, 0.5f, -0.3f);//3
+
+            Gl.glColor4f(1.0f, 1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.0f, 0.4f);//вершина
+            Gl.glColor4f(1.0f, 1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0f, 0.5f, -0.3f);
+            Gl.glColor4f(1.0f, 1.0f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.48f, 0.15f, -0.3f);
+
+            Gl.glColor4f(1.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.0f, 0.4f);//вершина
+            Gl.glColor4f(1.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.48f, 0.15f, -0.3f);
+            Gl.glColor4f(1.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.29f, -0.4f, -0.3f);
+
+            Gl.glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
+            Gl.glVertex3f(0.0f, 0.0f, 0.4f);//вершина
+            Gl.glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
+            Gl.glVertex3f(0.29f, -0.4f, -0.3f);
+            Gl.glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
+            Gl.glVertex3f(-0.29f, -0.4f, -0.3f);//1
+
+            Gl.glColor4f(1.0f, 0.5f, 0.0f, 0.0f);
+            Gl.glVertex3f(0.0f, 0.0f, 0.4f);//3
+            Gl.glColor4f(1.0f, 0.5f, 0.0f, 0.0f);
+            Gl.glVertex3f(-0.29f, -0.4f, -0.3f);//2
+            Gl.glColor4f(1.0f, 0.5f, 0.0f, 0.0f);
+            Gl.glVertex3f(-0.48f, 0.15f, -0.3f);//1
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glColor4f(0.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(-0.48f, 0.15f, -0.3f);
+            Gl.glColor4f(0.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0f, 0.5f, -0.3f);
+            Gl.glColor4f(0.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.48f, 0.15f, -0.3f);
+            Gl.glColor4f(0.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(0.29f, -0.4f, -0.3f);
+            Gl.glColor4f(0.0f, 0.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(-0.29f, -0.4f, -0.3f);
+            Gl.glEnd();
+            simpleOpenGlControl1.Invalidate();
+        }
+
+        void draw()
+        {
+            //куб
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glBlendFunc(Gl.GL_SRC_ALPHA, Gl.GL_ONE_MINUS_SRC_ALPHA);
+            Gl.glColor3f(0, 0, 0);
+            Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_LINE);
+
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex3f(0.5f, 0.5f, -0.5f);
+            Gl.glVertex3f(-0.5f, 0.5f, -0.5f);
+            Gl.glVertex3f(-0.5f, 0.5f, 0.5f);
+            Gl.glVertex3f(0.5f, 0.5f, 0.5f);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex3f(0.5f, -0.5f, 0.5f);
+            Gl.glVertex3f(-0.5f, -0.5f, 0.5f);
+            Gl.glVertex3f(-0.5f, -0.5f, -0.5f);
+            Gl.glVertex3f(0.5f, -0.5f, -0.5f);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex3f(0.5f, 0.5f, 0.5f);
+            Gl.glVertex3f(-0.5f, 0.5f, 0.5f);
+            Gl.glVertex3f(-0.5f, -0.5f, 0.5f);
+            Gl.glVertex3f(0.5f, -0.5f, 0.5f);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex3f(0.5f, -0.5f, -0.5f);
+            Gl.glVertex3f(-0.5f, -0.5f, -0.5f);
+            Gl.glVertex3f(-0.5f, 0.5f, -0.5f);
+            Gl.glVertex3f(0.5f, 0.5f, -0.5f);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex3f(-0.5f, 0.5f, 0.5f);
+            Gl.glVertex3f(-0.5f, 0.5f, -0.5f);
+            Gl.glVertex3f(-0.5f, -0.5f, -0.5f);
+            Gl.glVertex3f(-0.5f, -0.5f, 0.5f);
+            Gl.glEnd();
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex3f(0.5f, 0.5f, -0.5f);
+            Gl.glVertex3f(0.5f, 0.5f, 0.5f);
+            Gl.glVertex3f(0.5f, -0.5f, 0.5f);
+            Gl.glVertex3f(0.5f, -0.5f, -0.5f);
+            Gl.glEnd();
+
+            simpleOpenGlControl1.Invalidate();
+        }
+       
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void rotation(float x, float y, float z)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glClear(Gl.GL_DEPTH_BUFFER_BIT);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glPushMatrix();
+            Gl.glRotatef(5, x, y, z);
+            draw();
+            draw1();
+            simpleOpenGlControl1.Invalidate();
+        }
+
+        private void keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                Application.Exit();
+
+            if (e.KeyCode == Keys.NumPad1)
+                rotation(1f, 0, 0);
+            if (e.KeyCode == Keys.NumPad2)
+                rotation(0, 1f, 0);
+            if (e.KeyCode == Keys.NumPad3)
+                rotation(0, 0, 1f);
+
+            if (e.KeyCode == Keys.Space)
+                timer1.Stop();
+            if (e.KeyCode == Keys.Enter)
+                timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rotation(1f, 1f, 1f);
+        }
+
+        private void simpleOpenGlControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
